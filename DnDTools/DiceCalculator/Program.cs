a285fb49b6699d3d35599630c4fb51c8b70ccd57
Mikolaj Grobelny <miki4920@gmail.com>
2020-10-08 19:46:56 +0100

    Changed Structure of the project and added averages
    Created a console program for an average dice roll and changed structure of the repository.

---
+++ DnDTools/DiceCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonProgram
+{ 
+    class Program
+    {
+        static void Main()
+        {   while (true)
+            {
+                DiceAverageCalculator calculator = new DiceAverageCalculator();
+                Console.WriteLine("Please Insert dice equation: ");
+                string[] userInput = Console.ReadLine().ToLower().Replace(" ", "").Split("+");
+                Console.WriteLine("The average equals to {0}\n", calculator.CalculateAverage(userInput));
+            }
+        }
+    }
+    class DiceAverageCalculator
+    {
+        double CalculateDieAverage(string die)
+        {
+            string[] dieElements = die.Split("d");
+            int dieCount = Convert.ToInt32(dieElements[0]);
+            int dieValue = Convert.ToInt32(dieElements[1]);
+            return dieCount * ((1 + dieValue) / 2.0);
+            
+        }
+        public double CalculateAverage(string[] diceInput) {
+            double totalValue = 0;
+            int converter;
+            foreach (string die in diceInput)
+            {
+                bool canConvert = int.TryParse(die, out converter);
+                if (canConvert)
+                {
+                    totalValue += Convert.ToInt32(die);
+                }
+                else
+                {
+                    totalValue += CalculateDieAverage(die);
+                }
+            }
+            return totalValue;
+        }
+    }
+}
