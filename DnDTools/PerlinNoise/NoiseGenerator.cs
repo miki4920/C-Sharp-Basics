06c7d8ba32fc5be07cbe0ac4c19b7f1fbb973a56
Mikolaj Grobelny <miki4920@gmail.com>
2020-10-13 15:40:40 +0100

    Added a 2D noise
    TODO: Simplify the noise generator, move into one class and add polymorphism

---
+++ DnDTools/PerlinNoise/NoiseGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace PerlinNoise
+{
+    class NoiseGenerator
+    {
+        static void Main(string[] args)
+        {
+            PerlinNoise.Perlin2D noiseGenerator = new PerlinNoise.Perlin2D();
+            int noisePoint = 0;
+            int size = 1024;
+            double xInput;
+            double yInput;
+            Bitmap noise = new Bitmap(size, size);
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    xInput = x * 1.0 / size;
+                    yInput = y * 1.0 / size;
+                    noisePoint = (int)(noiseGenerator.OctavePerlin(xInput, yInput, 6, 1.5) * 256);
+                    noise.SetPixel(x, y, Color.FromArgb(noisePoint, noisePoint, noisePoint));
+                }
+            }
+            noise.Save("noise.bmp");
+        }
+    }
+}
---
+++ DnDTools/PerlinNoise/Perlin2D.cs
@@ -0,0 +1,127 @@
+﻿
+namespace PerlinNoise
+{
+    public class Perlin2D
+    {
+
+        public int repeat;
+
+        public Perlin2D(int repeat = -1)
+        {
+            this.repeat = repeat;
+        }
+
+        public double OctavePerlin(double x, double y, int octaves, double persistence)
+        {
+            double total = 0;
+            double frequency = 1;
+            double amplitude = 1;
+            double maxValue = 0;
+            for (int i = 0; i < octaves; i++)
+            {
+                total += perlin(x * frequency, y * frequency) * amplitude;
+
+                maxValue += amplitude;
+
+                amplitude *= persistence;
+                frequency *= 2;
+            }
+
+            return total / maxValue;
+        }
+
+        private static readonly int[] permutation = { 151,160,137,91,90,15,					
+		131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,	
+		190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+        88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+        77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+        102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+        135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+        5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+        223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+        129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+        251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+        49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+        138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+    };
+
+        private static readonly int[] p;                                                    
+
+        static Perlin2D()
+        {
+            p = new int[512];
+            for (int x = 0; x < 512; x++)
+            {
+                p[x] = permutation[x % 256];
+            }
+        }
+
+        public double perlin(double x, double y)
+        {
+            if (repeat > 0)
+            {
+                x = x % repeat;
+                y = y % repeat;
+            }
+
+            int xi = (int)x & 255;
+            int yi = (int)y & 255;
+            double xf = x - (int)x;
+            double yf = y - (int)y;
+            double u = fade(xf);
+            double v = fade(yf);
+
+            int aa, ab, ba, bb;
+            aa = p[p[xi] + yi];
+            ab = p[p[xi] + inc(yi)];
+            ba = p[p[inc(xi)] + yi];
+            bb = p[p[inc(xi)] + inc(yi)];
+
+            double x1, x2, y1;
+            x1 = lerp(grad(aa, xf, yf),                
+                        grad(ba, xf - 1, yf),             
+                        u);                                     
+            x2 = lerp(grad(ab, xf, yf - 1),                
+                        grad(bb, xf - 1, yf - 1),             
+                          u);
+            y1 = lerp(x1, x2, v);
+            return (y1 + 1) / 2;                     
+        }
+
+        public int inc(int num)
+        {
+            num++;
+            if (repeat > 0) num %= repeat;
+
+            return num;
+        }
+
+        public static double grad(int hash, double x, double y)
+        {
+            switch (hash % 8)
+            {
+                case 0x0: return x + y;
+                case 0x1: return -x + y;
+                case 0x2: return x - y;
+                case 0x3: return -x - y;
+                case 0x4: return y + x;
+                case 0x5: return y - x;
+                case 0x6: return -y - x;
+                case 0x7: return -y - x;
+                default: return 0;
+            }
+        }
+
+        public static double fade(double t)
+        {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+
+        public static double lerp(double a, double b, double x)
+        {
+            return a + x * (b - a);
+        }
+    }
+}
+
+
